903a7d787958dc8213fa333832f09dcd27bd3b13
SalmannA1 <1464334@stu.tmc.ac.uk>
2025-05-22 15:10:52 +0100

    Simplify website for beginners

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
-import Hero from './components/Hero';
 import About from './components/About';
 import TeamSection from './components/TeamSection';
 import JobsSection from './components/JobsSection';
-import ResourcesSection from './components/ResourcesSection';
 import ApplicationForm from './components/ApplicationForm';
-import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="bg-gray-100">
       <Header />
-      <Hero />
-      <About />
-      <TeamSection />
-      <Testimonials />
-      <JobsSection />
-      <ResourcesSection />
-      <ApplicationForm />
+      <main>
+        <About />
+        <TeamSection />
+        <JobsSection />
+        <ApplicationForm />
+      </main>
       <Footer />
     </div>
   );
